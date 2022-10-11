d7876a28b8c2b310180f26e43b47eacf03aaac70
Ya-Liang Chang (Allen) <allen.c@taboola.com>
2022-10-11 15:44:42 +0800

    Change version name and release url

--- addon/constants.py
+++ addon/constants.py
@@ -1,7 +1,7 @@
 VERSION = 'v1'
-RELEASE_URL = 'https://github.com/megachweng/Dict2Anki'
-VERSION_CHECK_API = 'https://api.github.com/repos/megachweng/Dict2Anki/releases/latest'
-MODEL_NAME = f'Dict2Anki-guofoo-{VERSION}'
+RELEASE_URL = 'https://github.com/amjltc295/Dict2Anki'
+VERSION_CHECK_API = 'https://api.github.com/repos/amjltc295/Dict2Anki/releases/latest'
+MODEL_NAME = f'Dict2Anki-AllenC-{VERSION}'
 
 BASIC_OPTION = ['definition', 'sentence', 'phrase', 'image', 'BrEPhonetic', 'AmEPhonetic']  # 順序和名稱不可修改
 EXTRA_OPTION = ['BrEPron', 'AmEPron', 'noPron']  # 順序和名稱不可修改
